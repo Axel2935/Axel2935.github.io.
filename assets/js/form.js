13e3dabbddec24babb4395833d81223687e8e1b7
Axel2935 <axelgrepra@gmail.com>
2023-10-14 18:24:37 -0300

    terminado basico del formulario

--- assets/js/form.js
+++ assets/js/form.js
@@ -12,7 +12,7 @@ document.getElementById('form')
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
       btn.value = 'Send Email';
-      alert('Sent!');
+    alert('Mensaje enviado con Exito');
     }, (err) => {
       btn.value = 'Send Email';
       alert(JSON.stringify(err));
